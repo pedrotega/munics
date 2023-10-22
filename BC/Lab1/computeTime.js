9b4ef95bf65aa346f7e718c969eba6deb2120515
poteroGIT <poteroGIT@github.com>
2023-10-22 13:57:15 +0200

    Files moved to Ej6

--- BC/Lab1/computeTime.js
+++
@@ -1,7 +0,0 @@
-const customDate = new Date(2023, 11, 31, 12, 0, 0, 0); // Año: 2023, Mes: diciembre (0-based), Día: 31, Hora: 11:00:00
-const currentUnixTimestamp = Math.floor(Date.now() / 1000); // Get the current Unix timestamp
-const newUnixTimestamp = currentUnixTimestamp + 60; // Add 5 minutes (300 seconds) 
-
-console.log("\nCurrent Unix Timestamp:", currentUnixTimestamp);
-console.log("Current Timestamp:", new Date(currentUnixTimestamp*1000));
-console.log("Unix Timestamp + 5 minutes:", newUnixTimestamp);
