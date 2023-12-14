2eca9890733efeec3e0e0c24ef6833e84e6ce097
pedrotega <potero@gradiant.org>
2023-12-14 13:01:21 +0100

    Código hecho no integrado

--- SI/Lab4/doubleRachet.py
+++ SI/Lab4/doubleRachet.py
@@ -1,3 +1,5 @@
+import paho.mqtt.client as mqtt
+from threading import Thread
 # GENERATE_DH, DH
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
 # KDF_RK, KDF_CK
@@ -18,6 +20,12 @@ from cryptography.hazmat.primitives import serialization
 
 # CONSTANTS
 MAX_SKIP = 10
+
+# MQTT broker address and port
+mqttBroker = '18.101.47.122'
+password = 'HkxNtvLB3GC5GQRUWfsA'
+mqtt_id = 'sinf'
+port = 1883
 
 class DH_pair:
     def __init__(self, dh_priv, dh_pub) -> None:
@@ -43,6 +51,8 @@ class HEADER:
         self.pn = int.from_bytes(header_bytes[32:33], byteorder='big')
         self.n = int.from_bytes(header_bytes[33:34], byteorder='big')
 
+def CONCAT(ad: bytes, header: HEADER):
+    return ad + header.to_bytes()
 
 def GENERATE_DH():
     # Generate a new DH pair key.
@@ -78,15 +88,15 @@ def ENCRYPT(mk: bytes, plaintext: bytes, assotiate_data: bytes):
     # Key Derivation Function
     output_length = 80
     prk = hkdf_extract(salt=bytes(output_length), input_key_material=mk, hash=hashlib.sha512)
-    exp = hkdf_expand(pseudo_random_key=prk, info=b"Info ENCRYPT", length=output_length, hash=hashlib.sha512)
+    exp = hkdf_expand(pseudo_random_key=prk, info=b"Info ENCRYPT/DECRYPT", length=output_length, hash=hashlib.sha512)
 
-    ek = exp[:32] # Encryption Key
+    sk = exp[:32] # Encryption Key
     ak = exp[32:64] # Authentication Key
     iv = exp[64:] # Initialization vector
 
     # Construct an AES-128-GCM Cipher object with the generated encryption key and IV.
     encryptor = Cipher(
-            algorithms.AES(key=ek),
+            algorithms.AES(key=sk),
             modes.GCM(initialization_vector=iv),
             backend=default_backend()
         ).encryptor()
@@ -98,76 +108,140 @@ def ENCRYPT(mk: bytes, plaintext: bytes, assotiate_data: bytes):
 
     # HMAC for authentication of the associate it data.
     ad = hmac.new(key=ak, msg=assotiate_data, digestmod=hashlib.sha512).digest() # 512b = 64B
-    print(len(ad))
-    
+
     return tag + ad + ciphertext
 
-def CONCAT(ad: bytes, header: HEADER):
+def DECRYPT(mk, ciphertext, assotiate_data):
+    tag = ciphertext[:16]
+    ad_original = ciphertext[16:16+64]
+    ciphertext = ciphertext[16+64:]
+
+    # Key Derivation Function
+    output_length = 80
+    prk = hkdf_extract(salt=bytes(output_length), input_key_material=mk, hash=hashlib.sha512)
+    exp = hkdf_expand(pseudo_random_key=prk, info=b"Info ENCRYPT", length=output_length, hash=hashlib.sha512)
+
+    sk = exp[:32] # Encryption Key
+    ak = exp[32:64] # Authentication Key
+    iv = exp[64:] # Initialization vector
+
+    decryptor = Cipher(
+        algorithms.AES(key=sk),
+        modes.GCM(initialization_vector=iv, tag=tag),
+        backend=default_backend()
+    ).decryptor()
+
+    # Decryption and unpadding.
+    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    plaintext = unpadder.update(plaintext) + unpadder.finalize()
+
+    # HMAC for authentication of the associate it data.
+    ad = hmac.new(key=ak, msg=assotiate_data, digestmod=hashlib.sha512).digest() # 512b = 64B
+
+    if ad != ad_original:
+        raise Exception("Error authenticating data.")
     
+    return plaintext
 
-    return ad + header
+class User:
+    def __init__(self, Alice: bool, sk, dh_bob) -> None:
+        if Alice:
+            self.RatchetInitAlice(sk, dh_bob)
+        else:
+            self.RatchetInitBob(sk, dh_bob)
+    
+    def RatchetInitAlice(self, SK, bob_dh_public_key):
+        self.DHs = GENERATE_DH()
+        self.DHr = bob_dh_public_key
+        self.RK, self.CKs = KDF_RK(SK, DH(self.DHs, self.DHr)) 
+        self.CKr = None
+        self.Ns = 0
+        self.Nr = 0
+        self.PN = 0
+        self.MKSKIPPED = {}
 
-def RatchetDecrypt(self, header, ciphertext, AD):
-    plaintext = TrySkippedMessageKeys(self, header, ciphertext, AD)
-    if plaintext != None:
-        return plaintext
-    if header.dh != self.DHr:                 
-        SkipMessageKeys(self, header.pn)
-        DHRatchet(self, header)
-    SkipMessageKeys(self, header.n)             
-    self.CKr, mk = KDF_CK(self.CKr)
-    self.Nr += 1
-    return DECRYPT(mk, ciphertext, CONCAT(AD, header))
+    def RatchetInitBob(self, SK, bob_dh_key_pair):
+        self.DHs = bob_dh_key_pair
+        self.DHr = None
+        self.RK = SK
+        self.CKs = None
+        self.CKr = None
+        self.Ns = 0
+        self.Nr = 0
+        self.PN = 0
+        self.MKSKIPPED = {}
 
-def TrySkippedMessageKeys(self, header, ciphertext, AD):
-    if (header.dh, header.n) in self.MKSKIPPED:
-        mk = self.MKSKIPPED[header.dh, header.n]
-        del self.MKSKIPPED[header.dh, header.n]
+    def RatchetDecrypt(self, header, ciphertext: bytes, AD: bytes):
+        """ header = HEADER()
+        header.to_object(header_bytes=header_bytes) """
+        plaintext = self.TrySkippedMessageKeys(self, header, ciphertext, AD)
+        if plaintext != None:
+            return plaintext
+        if header.dh != self.DHr:                 
+            self.SkipMessageKeys(self, header.pn)
+            self.DHRatchet(self, header)
+        self.SkipMessageKeys(self, header.n)             
+        self.CKr, mk = KDF_CK(self.CKr)
+        self.Nr += 1
         return DECRYPT(mk, ciphertext, CONCAT(AD, header))
-    else:
-        return None
 
-def SkipMessageKeys(self, until):
-    if self.Nr + MAX_SKIP < until:
-        Exception("SkipMessage exception.")
-    if self.CKr != None:
-        while self.Nr < until:
-            self.CKr, mk = KDF_CK(self.CKr)
-            self.MKSKIPPED[self.DHr, self.Nr] = mk
-            self.Nr += 1
+    def TrySkippedMessageKeys(self, header: HEADER, ciphertext: bytes, AD: bytes):
+        if (header.dh, header.n) in self.MKSKIPPED:
+            mk = self.MKSKIPPED[header.dh, header.n]
+            del self.MKSKIPPED[header.dh, header.n]
+            return DECRYPT(mk, ciphertext, CONCAT(AD, header))
+        else:
+            return None
 
-def DHRatchet(self, header):
-    self.PN = self.Ns                          
-    self.Ns = 0
-    self.Nr = 0
-    self.DHr = header.dh
-    self.RK, self.CKr = KDF_RK(self.RK, DH(self.DHs, self.DHr))
-    self.DHs = GENERATE_DH()
-    self.RK, self.CKs = KDF_RK(self.RK, DH(self.DHs, self.DHr))
+    def SkipMessageKeys(self, until):
+        if self.Nr + MAX_SKIP < until:
+            Exception("SkipMessage exception.")
+        if self.CKr != None:
+            while self.Nr < until:
+                self.CKr, mk = KDF_CK(self.CKr)
+                self.MKSKIPPED[self.DHr, self.Nr] = mk
+                self.Nr += 1
 
-def RatchetEncrypt(self, plaintext, AD):
-    self.CKs, mk = KDF_CK(self.CKs)
-    header = HEADER(self.DHs, self.PN, self.Ns)
-    self.Ns += 1
-    return header, ENCRYPT(mk, plaintext, CONCAT(AD, header))
+    def DHRatchet(self, header):
+        self.PN = self.Ns                          
+        self.Ns = 0
+        self.Nr = 0
+        self.DHr = header.dh
+        self.RK, self.CKr = KDF_RK(self.RK, DH(self.DHs, self.DHr))
+        self.DHs = GENERATE_DH()
+        self.RK, self.CKs = KDF_RK(self.RK, DH(self.DHs, self.DHr))
 
-def RatchetInitAlice(self, SK, bob_dh_public_key):
-    self.DHs = GENERATE_DH()
-    self.DHr = bob_dh_public_key
-    self.RK, self.CKs = KDF_RK(SK, DH(self.DHs, self.DHr)) 
-    self.CKr = None
-    self.Ns = 0
-    self.Nr = 0
-    self.PN = 0
-    self.MKSKIPPED = {}
+    def RatchetEncrypt(self, plaintext, AD):
+        self.CKs, mk = KDF_CK(self.CKs)
+        header = HEADER(self.DHs, self.PN, self.Ns)
+        self.Ns += 1
+        return header, ENCRYPT(mk, plaintext, CONCAT(AD, header))
 
-def RatchetInitBob(self, SK, bob_dh_key_pair):
-    self.DHs = bob_dh_key_pair
-    self.DHr = None
-    self.RK = SK
-    self.CKs = None
-    self.CKr = None
-    self.Ns = 0
-    self.Nr = 0
-    self.PN = 0
-    self.MKSKIPPED = {}
+def publisher():
+    client = mqtt.Client()
+    client.username_pw_set(username=mqtt_id, password=password)
+    client.connect(mqttBroker)
+
+    while True:
+        m = input("\nMensaje: ")
+        next_hop, c = nested_hybrid_encryption(path=path, m=m)
+        client.publish(next_hop, c)
+        print("\nJust published " + str(c) + " to topic "+ next_hop + ".")
+
+def on_message(client, userdata, message):
+    m = decode_and_relay_messages(getKey(privkeydictionary[my_id]), message.payload)
+
+def subscriber():
+    client = mqtt.Client()
+    client.username_pw_set(username=mqtt_id, password=password)
+    client.connect(mqttBroker)
+
+    client.subscribe(my_id)
+    client.on_message = on_message
+    client.loop_forever()
+
+t_publisher = Thread(target=publisher)
+t_subscriber = Thread(target=subscriber)
+t_publisher.start()
+t_subscriber.start()
